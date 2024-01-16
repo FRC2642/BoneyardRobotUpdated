c0aa526c9309f6f3d21ddcbc99533624631ac2c4
CommanderCoder15 <superzoom15.5@gmail.com>
2024-01-15 19:04:18 -0500

    update

--- src/main/java/frc/robot/Subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/Subsystems/MotorSubsystem.java
@@ -5,15 +5,23 @@
 package frc.robot.Subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAnalogSensor;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAnalogSensor.Mode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MotorSubsystem extends SubsystemBase {
   private CANSparkMax motor1 = new CANSparkMax(0, MotorType.kBrushless);
 
+  private SparkMaxAnalogSensor absEncoder1 = motor1.getAnalog(Mode.kAbsolute);
+
   public void runMotor(double motorspeed) {
     motor1.set(motorspeed);
+  }
+
+  public double getEncoder1Value() {
+    return absEncoder1.getPosition();
   }
 
   /** Creates a new MotorSubsystem. */
